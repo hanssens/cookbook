5b8d596e98761f9e27a6d7e06ffbaf09616851e5
Juliën Hanssens <j@hanssens.com>
2013-09-03 12:50:34 +0200

    Basic setup for show-edit-save as partial view

--- Source/Hanssens CookBook/Hanssens.CookBook/Areas/Samples/Controllers/AjaxController.cs
+++ Source/Hanssens CookBook/Hanssens.CookBook/Areas/Samples/Controllers/AjaxController.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
+using Hanssens.CookBook.Areas.Samples.Models;
 
 namespace Hanssens.CookBook.Areas.Samples.Controllers
 {
     public class AjaxController : Controller
     {
-        //
-        // GET: /Samples/Ajax/
 
         public ActionResult Index()
         {
-            return View();
+            var model = new Customer();
+            return View(model);
+        }
+
+        public ActionResult SavePartial(Customer model)
+        {
+            return PartialView("_EditCustomer", model);
         }
 
     }
---
+++ Source/Hanssens CookBook/Hanssens.CookBook/Areas/Samples/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hanssens.CookBook.Areas.Samples.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        
+        [Required, Display(Name = "Customer name")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Date of birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
