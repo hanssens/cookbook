11236fbc388285f9559065befe6c8014b5faa749
Juliën Hanssens <j@hanssens.com>
2013-09-03 12:43:32 +0200

    Added "Samples" area

---
+++ Source/Hanssens CookBook/Hanssens.CookBook/Areas/Samples/Controllers/AjaxController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hanssens.CookBook.Areas.Samples.Controllers
+{
+    public class AjaxController : Controller
+    {
+        //
+        // GET: /Samples/Ajax/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
